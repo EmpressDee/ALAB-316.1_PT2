d1212e3fa61b080c41217d9a373f6355aaddd3bf
Claudena Daniel <cjdaniel0530@gmail.com>
2026-05-21 21:05:24 -0400

    first commit

---
+++ ALAB-316_1_1.js
@@ -0,0 +1,48 @@
+//import "./styles.css"; 
+// Part 1: Getting Started 
+
+// 1.
+let mainEl = document.querySelector("main") ;
+
+// 2.
+mainEl.style.backgroundColor = "var(--main-bg)";
+
+// 3. 
+
+mainEl.innerHTML = `<h1> DOM Manipulation </h1>`
+
+// 4. 
+
+mainEl.classList.add("flex-ctr");
+
+
+
+
+
+// //////////////////////////////////////////////
+// Part 2: Creating Menu Bar 
+
+// 1. 
+
+let topMenuEL = document.getElementById("top-menu");
+
+// 2. 
+
+topMenuEL.style.height = "100%";
+
+// 3.
+
+topMenuEL.style.backgroundColor = " var( --top-menu-bg) "
+
+// 4.
+
+topMenuEL.classList.add("flex-around");
+
+
+
+
+
+//////////////////////////////////////////////////////////
+
+
+
